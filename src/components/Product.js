b93d6c3370b442403fe53650abd1b308e562849a
Juan <juanm.fernandez@hotmail.com>
2022-05-11 18:45:13 -0300

    Card user 2

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
+import { Link, useParams } from "react-router-dom";
+import { BiEdit, BiTrash } from "react-icons/bi";
 
 function Product(){
     const params = useParams();
@@ -33,31 +33,28 @@ function Product(){
     return(
         <>
             <div className="producto">
-                <div className="detalleProducto">
-                    <div className="imagenProducto">
-                        <img src={`http://localhost:3030/${product.image}`} alt="" />
+                <div className="card">
+                    <img src={`http://localhost:3030/${product.image}`} className="card-img-top" alt="" />
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">Marca: {product.marca}</li>
+                        <li className="list-group-item">Talle: {product.talle}</li>
+                        <li className="list-group-item">u$s {product.precio}</li>
+                        <li className="list-group-item">Stock: {product.stock}</li>
+                    </ul>
+                    <div className="card-body">
+                        <h5 className="card-title">{product.id} {product.nombre}</h5>
+                        <p className="card-text">{product.descripcion}.</p>
                     </div>
-
-                    <div className="detalle">
-                        <h2>{product.nombre}</h2>
-                        <h3>{product.marca}</h3>
-                        <div className="descripcion">
-                            <h2>DESCRIPCIÓN</h2>
-                            <p>{product.descripcion}</p>
-                        </div>                
-                        <label>Precio USD {product.precio} </label>
-                        
-                        <div className="talles">
-                            <label htmlFor="talles">Talles  </label>       
-                        </div>
-                        <div>                
-                            <select name="talles" id="talles" className="talles">
-                                <option value>Elegí una opción...</option>
-                                        <option value="<%= producto.talle %>">{product.talle}</option>
-                            </select>
-                        </div>
-
+                    <div className="card-body">
+                        <Link to={`/product/${product.id}`} className="card-link">
+                            <BiEdit className="icon"/>
+                        </Link>
+                        <Link to={`/product/${product.id}`} className="card-link">
+                            <BiTrash className="icon"/>
+                        </Link>
                     </div>
+                </div>
+                <div className="table-responsive" style={{'overflow-x': 'auto'}}>
                 </div>
             </div>
         </>
